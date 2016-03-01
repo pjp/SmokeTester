0682395491c14d92390e88527b16b3c0151c4078
ppearce <paul@pearceful.net>
2016-03-01 20:23:55 +0000

    Parsed more JSON

--- src/main/java/com/pearceful/util/standalone/StandaloneJsonConfig.java
+++ src/main/java/com/pearceful/util/standalone/StandaloneJsonConfig.java
@@ -1,22 +1,22 @@
 package com.pearceful.util.standalone;
 
+import com.pearceful.util.SmokeTestContext;
+
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by ppearce on 2016-02-29.
  */
 public class StandaloneJsonConfig {
 
-    private JsonSetup setup;
-    private List<JsonTestDefinition> testDefinitions = new ArrayList<>();
+    public JsonSetup setup;
+    public List<JsonTestDefinition> testDefinitions = new ArrayList<>();
     private String[] cmdLineArgs;
     private String tag;
     private String configFile;
@@ -48,7 +48,11 @@ public class StandaloneJsonConfig {
 
         Map config = (Map)result;
 
-        setup   = new JsonSetup((Map<String, Object>)config.get("config"));
+        setup   = new JsonSetup(
+                configFile,
+                tag,
+                cmdLineArgs,
+                (Map<String, Object>)config.get("setup"));
 
         Map<String, Map<String, Object>> tests = (Map<String, Map<String, Object>>)config.get("test");
         for(Map.Entry<String, Map<String, Object>> entry : tests.entrySet()) {
@@ -63,13 +67,8 @@ public class StandaloneJsonConfig {
     public static class JsonSetup {
         private int timeoutSecondsForAllTests;
         private int threadPoolSize;
-        private Map<String, String> systemVariables;
+        private Map<String, String> systemVariables = new HashMap<>();
 
-        public static enum VALUE_FROM {
-            INTERNAL,
-            INLINE_VALUE,
-            INLINE_VALUE_FROM_MATCHING_TAG
-        }
         public static enum INTERNAL_VALUE_NAMES {
             VERSION,
             CONFIG_FILE,
@@ -81,8 +80,88 @@ public class StandaloneJsonConfig {
             TAG
         }
 
-        public JsonSetup(final Map<String, Object>config) {
+        public JsonSetup(
+                final String configFile,
+                final String tag,
+                final String[] cmdLineArgs,
+                final Map<String, Object>config) {
+
+            timeoutSecondsForAllTests                   = (int)config.get("timeout_seconds_for_all_tests");
+            threadPoolSize                              = (int)config.get("thread_pool_size");
+            List<Map<String, Object>> systemVars        = (List<Map<String, Object>>)config.get("system_variables");
+
+            for(Map<String, Object>systemVar : systemVars) {
+                String varName = (String)systemVar.get("name");
+
+                String varVal  = (String)systemVar.get("internal_value");
+                if(null != varVal) {
+                    switch(varVal) {
+                        case "VERSION":
+                            systemVariables.put(varName, SmokeTestContext.VERSION);
+                            break;
+                        case "CONFIG_FILE":
+                            systemVariables.put(varName, configFile);
+                            break;
+                        case "CMD_LINE":
+                            systemVariables.put(varName, cmdLineArgs.toString());
+                            break;
+                        case "TIMEOUT":
+                            systemVariables.put(varName, "" + timeoutSecondsForAllTests);
+                            break;
+                        case "THREAD_POOL_SIZE":
+                            systemVariables.put(varName, "" + threadPoolSize);
+                            break;
+                        case "ID":
+                            systemVariables.put(varName,"ID-PLACEHOLDER");
+                            break;
+                        case "OS":
+                            String osName   = System.getProperty("os.name");
+                            osName          = osName.toLowerCase(Locale.ENGLISH);
+
+                            if (osName.indexOf("windows") != -1) {
+                                systemVariables.put(varName, "windows");
+                            } else {
+                                systemVariables.put(varName, "unix");
+                            }
+                            break;
+                        case "TAG":
+                            systemVariables.put(varName, tag);
+                            break;
+                    }
+                    continue;
+                }
+
+                varVal  = (String)systemVar.get("inline_value") ;
+                if(null != varVal) {
+                    systemVariables.put(varName, varVal);
+                    continue;
+                }
+
+//                varVal  = (String)systemVar.get("inline_value_from_matching_tag");
+//                if(null != varVal) {
+//                    continue;
+//                }
+            }
         }
+
+        public int getTimeoutSecondsForAllTests() {
+            return timeoutSecondsForAllTests;
+        }
+
+        public int getThreadPoolSize() {
+            return threadPoolSize;
+        }
+
+        public Map<String, String> getSystemVariables() {
+            return systemVariables;
+        }
+
+        public static enum VALUE_FROM {
+            INTERNAL,
+            INLINE_VALUE,
+            INLINE_VALUE_FROM_MATCHING_TAG
+        }
+
     }
 
     public static class JsonTestDefinition {
--- src/test/java/com/pearceful/util/standalone/StandaloneJsonConfigParseTest.java
+++ src/test/java/com/pearceful/util/standalone/StandaloneJsonConfigParseTest.java
@@ -20,4 +20,27 @@ public class StandaloneJsonConfigParseTest extends TestCase {
 
         assertNotNull(config);
     }
+
+    public void testReadConfigSetup() throws IOException, ScriptException {
+        String[] cmdLineArgs    = {"a", "b", "c"};
+        String tag              = "DEV";
+
+        StandaloneJsonConfig config =
+                new StandaloneJsonConfig(jsonFile, cmdLineArgs, tag);
+
+        assertEquals(600, config.setup.getTimeoutSecondsForAllTests());
+        assertEquals(2,   config.setup.getThreadPoolSize());
+        assertEquals(10,  config.setup.getSystemVariables().size());
+    }
+
+    public void testReadConfigTest() throws IOException, ScriptException {
+        String[] cmdLineArgs    = {"a", "b", "c"};
+        String tag              = "DEV";
+
+        StandaloneJsonConfig config =
+                new StandaloneJsonConfig(jsonFile, cmdLineArgs, tag);
+
+        assertEquals(4, config.testDefinitions.size());
+
+    }
 }
