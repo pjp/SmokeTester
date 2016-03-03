30dc41f6c5c62190cc27355e8c828f5f0717e105
ppearce <paul@pearceful.net>
2016-03-03 09:19:42 +0000

    Version update ready for release

--- standalone/jrun.sh
+++ standalone/jrun.sh
@@ -1,3 +1,3 @@
-UNIX_CLASSPATH=../target/SmokerTester-1.1-SNAPSHOT.jar:log4j-1.2.17.jar
+UNIX_CLASSPATH=../target/SmokerTester-1.1.jar:log4j-1.2.17.jar
 
 java -cp $UNIX_CLASSPATH -Dlog4j.configuration=file:log4j.xml com.pearceful.util.standalone.JsonConfigProcessor sample-conf.json $1 $2
--- standalone/run.sh
+++ standalone/run.sh
@@ -1,3 +1,3 @@
-UNIX_CLASSPATH=../target/SmokerTester-1.0:log4j-1.2.17.jar:gson-2.6.2.jar
+UNIX_CLASSPATH=../target/SmokerTester-1.1.jar:log4j-1.2.17.jar:gson-2.6.2.jar
 
 java -cp $UNIX_CLASSPATH -Dlog4j.configuration=file:log4j.xml com.pearceful.util.standalone.ShellScriptListProcessor scripts.txt $1 $2
