36ad300ebb0d62067830d32b5777380a91537dc0
ppearce <paul@pearceful.net>
2016-03-09 13:41:33 +0000

    Version update

--- standalone/jrun.cmd
+++ standalone/jrun.cmd
@@ -1,3 +1,3 @@
-set DOS_CLASSPATH=../target/SmokerTester-1.1.jar;log4j-1.2.17.jar
+set DOS_CLASSPATH=../target/SmokerTester-1.2.jar;log4j-1.2.17.jar
 
 java -cp %DOS_CLASSPATH% -Dlog4j.configuration=file:log4j.xml com.pearceful.util.standalone.JsonConfigProcessor sample-conf.json %1 %2
